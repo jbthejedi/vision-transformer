f39878c873cbeded60c3e90cb22f6aa7b04c4d6b
Justin Barry <justinbarry@Justins-MacBook-Air.local>
2025-01-27 16:47:58 +0700

    vit_adapted works

--- vit_practice.py
+++ vit_practice.py
@@ -14,8 +14,9 @@ from torchvision.datasets import MNIST
 class LayerNorm(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.weight = nn.Parameter(ndim=config.n_embd)
-        self.bias = nn.Parameter(ndim=config.n_embd)
+        self.weight = nn.Parameter(torch.ones(config.n_embd))
+        self.bias = nn.Parameter(torch.zeros(config.n_embd)) if config.bias else None
 
     def forward(self, x):
-        x = torch.layer_norm(x, self.weight.shape, self.weight, self.bias, lr=1e-3)
+        x = torch.layer_norm(x, x.shape[-1:], self.weight, self.bias, eps=1e-3)
+        
