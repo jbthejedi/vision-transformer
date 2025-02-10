bcb1f105664a2d8b7e87eb90c335066f1e6641bd
Justin Barry <justinbarrythejedi@gmail.com>
2025-02-10 10:07:17 +0000

    changed hyperparams

--- src/vit_runpod.py
+++ src/vit_runpod.py
@@ -16,17 +16,17 @@ torch.manual_seed(1337)
 
 @dataclass
 class Config:
-    batch_size : int = 128
+    batch_size : int = 256
     patch_size : int = 4
     n_embd : int = 256
     n_channels : int = 3
     n_heads : int = 8
     # normalize_shape : tuple = (0.5)
     normalize_shape : tuple = (0.5, 0.5, 0.5)
-    n_blocks : int = 4
+    n_blocks : int = 8
     p_dropout : float = 0.2
 
-    n_epochs : int = 20
+    n_epochs : int = 100
     image_size : int = 32
     n_classes : int = 10
     p_train_split : float = 0.9
