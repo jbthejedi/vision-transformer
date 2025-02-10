4a5d96410c052056d79b67408874e15c27861575
Justin Barry <justinbarrythejedi@gmail.com>
2025-02-10 16:54:32 +0700

    scaling up

--- src/vit_runpod.py
+++ src/vit_runpod.py
@@ -16,17 +16,17 @@ torch.manual_seed(1337)
 
 @dataclass
 class Config:
-    batch_size : int = 32
+    batch_size : int = 128
     patch_size : int = 4
-    n_embd : int = 32
+    n_embd : int = 256
     n_channels : int = 3
-    n_heads : int = 2
+    n_heads : int = 8
     # normalize_shape : tuple = (0.5)
     normalize_shape : tuple = (0.5, 0.5, 0.5)
-    n_blocks : int = 2
+    n_blocks : int = 4
     p_dropout : float = 0.2
 
-    n_epochs : int = 10
+    n_epochs : int = 20
     image_size : int = 32
     n_classes : int = 10
     p_train_split : float = 0.9
